dc7aaf082b09aac40f320c1acc589b39901b50e2
William Millette <wmillett@c2r1p2.42quebec.com>
2023-11-27 20:10:03 -0500

    fixed exit handle

--- src/garbage/handle_exit.c
+++ src/garbage/handle_exit.c
@@ -6,11 +6,12 @@
 /*   By: wmillett <wmillett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 21:21:45 by wmillett          #+#    #+#             */
-/*   Updated: 2023/11/26 21:38:58 by wmillett         ###   ########.fr       */
+/*   Updated: 2023/11/27 20:01:31 by wmillett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "global.h"
+#include "error.h"
 
 void err_leave_shell(char *err_message)
 {
