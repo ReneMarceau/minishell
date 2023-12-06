eb1f641fe1949b2ad7350817a9bbea2779478007
William Millette <wmillett@c1r5p5.42quebec.com>
2023-12-05 21:10:12 -0500

    add rm_quote

--- src/expand/expand_quote.c
+++ src/expand/expand_quote.c
@@ -6,24 +6,39 @@
 /*   By: wmillett <wmillett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 20:25:52 by wmillett          #+#    #+#             */
-/*   Updated: 2023/12/05 20:34:15 by wmillett         ###   ########.fr       */
+/*   Updated: 2023/12/05 21:00:35 by wmillett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parse.h"
 
-static void remove_quote(t_token *current, t_shell *shell)
+bool rm_quote_str(char *current, t_shell *shell)
 {
 	size_t i;
+	int quote;
 
+	quote = 0;
 	i = 0;
-	while (current->token[i])
+	while (current[i])
 	{
-		if (ft_isquote(current->token[i]))
-			rm_ext(current, i, 1, shell);
+		if (ft_isquote(current[i]))
+		{
+			quote = current[i];
+			rm_ext_here(current, i, 1, shell);
+		}
 		if (shell->mem_err_flg == TRUE)
-			return ;
+			return (FALSE);
+		if (quote)
+		{
+			while(current[i] && current[i] != quote)
+				i++;
+			rm_ext_here(current, i, 1, shell);
+			quote = FALSE;
+		}
+		if (shell->mem_err_flg == TRUE)
+			return (FALSE);
 	}
+	return (TRUE);
 }
 
 bool rm_quotes(t_token *head, t_shell *shell)
@@ -36,7 +51,7 @@ bool rm_quotes(t_token *head, t_shell *shell)
 	while (current)
 	{
 		if (prev_here == FALSE && current->type == STR && is_there_quote(current->token))
-			remove_quote(current, shell);
+			rm_quote_str(current->token, shell);
 		if (shell->mem_err_flg == TRUE)
 			return (FALSE);
 		if (current->type == HEREDOC)
